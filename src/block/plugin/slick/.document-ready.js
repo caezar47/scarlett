b48d95379dff47ab5086cc16d82fcf6841f8080d
caezar47 <iss47@ya.ru>
2020-12-23 14:09:37 +0300

    Добавил insta блок

--- src/block/plugin/slick/.document-ready.js
+++ src/block/plugin/slick/.document-ready.js
@@ -1,6 +1,7 @@
 'use strict';
 $(function() { 
 	var slider = $('[data-slick-slider]');
+	var insta = $('[data-slick-insta]');
 	var prevArrow = '<button type="button" class="slick-btn  is--prev"><span class="sr-only">Предыдущий слайд</span></button>';
 	var nextArrow = '<button type="button" class="slick-btn  is--next"><span class="sr-only">Следующий слайд</span></button>';
 	slider.slick({
@@ -12,8 +13,7 @@ $(function() {
 		variableWidth: true,
 		prevArrow: prevArrow,
 		nextArrow: nextArrow,
-		responsive: [
-		  
+		responsive: [		  
 		    {
 				breakpoint: 1200,
 				settings: {
@@ -38,6 +38,40 @@ $(function() {
 					variableWidth: false,
 					slidesToShow: 2,
 					slidesToScroll: 2, 
+				}
+		    },
+		    {
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1, 
+				}
+		    }
+		]
+	});
+	insta.slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: false,
+		infinite: true,
+		variableWidth: true, 
+		prevArrow: prevArrow,
+		nextArrow: nextArrow,
+		responsive: [		  
+		    {
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1, 
+				}
+		    },
+		  
+		    {
+				breakpoint: 766,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1, 
 				}
 		    },
 		    {
