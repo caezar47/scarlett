736b379826e38c31fe127acb2f62690dda940078
caezar47 <iss47@ya.ru>
2021-06-10 14:06:42 +0300

    правки

--- src/block/common/form/.plugin.js
+++ src/block/common/form/.plugin.js
@@ -2168,7 +2168,7 @@
             $.validationEngineLanguage.allRules = {
                 "required": { // Add your regex rules here, you can take telephone as an example
                     "regex": "none",
-                    "alertText": "* Необходимо заполнить",
+                    "alertText": "Необходимо заполнить. ",
                     "alertTextCheckboxMultiple": "* Вы должны выбрать вариант",
                     "alertTextCheckboxe": "* Необходимо отметить"
                 },
@@ -2233,7 +2233,7 @@
                 "email": {
                     // Shamelessly lifted from Scott Gonzalez via the Bassistance Validation plugin http://projects.scottsplayground.com/email_address_validation/
                     "regex": /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
-                    "alertText": "* Неверный формат email"
+                    "alertText": "Неверный формат email. "
                 },
                 "integer": {
                     "regex": /^[\-\+]?\d+$/,
@@ -2274,7 +2274,7 @@
                 },
                 "onlyLetterSp": {
                     "regex": /^[a-zA-Z\u0400-\u04FF\ \']+$/,
-                    "alertText": "* Только буквы"
+                    "alertText": "Только буквы. "
                 },
                 "onlyLetterNumber": {
                     "regex": /^[0-9a-zA-Z\u0400-\u04FF]+$/,
